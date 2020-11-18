6d55bead00784e4a828bd373f3f3da53b1a3b1ff
Önder Koşar <onderkosar@hotmail.com>
2020-11-18 15:15:52 +0300

    Initial Commit

---
+++ SE-AppleFrameworks/SE_AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SE_AppleFrameworksApp.swift
+//  SE-AppleFrameworks
+//
+//  Created by Önder Koşar on 18.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SE_AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
