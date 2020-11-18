635d23ee390f70353164b1357c82e00d183fc5b1
Önder Koşar <onderkosar@hotmail.com>
2020-11-18 15:37:53 +0300

    assets are imported, model is created

---
+++ SE-AppleFrameworks/FrameworkGridView.swift
@@ -0,0 +1,38 @@
+//
+//  FrameworkGridView.swift
+//  SE-AppleFrameworks
+//
+//  Created by Önder Koşar on 18.11.2020.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
+
+struct FrameworkTitleView: View {
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
