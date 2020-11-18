9511a2e35858b7b1015bc05794b3a9af0f140083
Önder Koşar <onderkosar@hotmail.com>
2020-11-19 00:30:58 +0300

    refactor in project organization

---
+++ SE-AppleFrameworks/Views/Buttons/XDismissButton.swift
@@ -0,0 +1,35 @@
+//
+//  XDismissButton.swift
+//  SE-AppleFrameworks
+//
+//  Created by Önder Koşar on 19.11.2020.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    
+    @Binding var isShowingDetailView: Bool
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            Button {
+                isShowingDetailView = false
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(Color(.label))
+                    .imageScale(.large)
+                    .frame(width: 44, height: 44)
+            }
+        }
+        .padding()
+    }
+}
+
+struct XDismissButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XDismissButton(isShowingDetailView: .constant(false))
+    }
+}
